d5841dbaa722afde10752d1adb4b451741302851
bournafide <bournejocelynn@gmail.com>
2019-04-17 19:01:47 +1200

    login activity commit

--- app/src/main/java/com/example/alison/autprime/MainActivity.java
+++ app/src/main/java/com/example/alison/autprime/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.alison.autprime;
-//REMEMBER, THIS IS THE MASTER BRANCH, DON'T EDIT OR PUSH ANYTHING TO IT UNTIL A CONSENSUS IS REACHED
-//CREATE A NEW BRANCH FOR THE ACTIVITY OR FUNCTION OF THE APP YOU WILL BE DOING
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
