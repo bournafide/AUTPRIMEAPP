c9ddde85e78bfc0799d380fabd971ab08d6af3e7
bournafide <bournejocelynn@gmail.com>
2019-04-25 01:04:50 +1200

    test redirecting user to next activity after successful login

---
+++ app/src/main/java/com/example/alison/autprime/Activity2.java
@@ -0,0 +1,12 @@
+package com.example.alison.autprime;
+//this activity will be what the user sees if they login successfully
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
--- app/src/main/java/com/example/alison/autprime/MainActivity.java
+++ app/src/main/java/com/example/alison/autprime/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.alison.autprime;
-
+//when user logs in successfully, they will be directed to the homepage (Activity2.java)
+//no security measures have been implemented yet
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button login_button;
+    private EditText username, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = findViewById(R.id.editText2);
+        password = findViewById(R.id.editText);
+        login_button = findViewById(R.id.button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //need to add code to verify user login details
+                //is correct with microsoft outlook before homepage is opened
+                validate(username.getText().toString(),password.getText().toString());
+            }
+        });
+    }
+    public void validate(String username, String password){
+        if(username.equals("user") && password.equals("pass")){
+            //print out non-error message and startup the next activity after login successful
+            Toast.makeText(getApplicationContext(),"Login successful",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, Activity2.class);
+            startActivity(intent);
+        }
+        else{
+            //print out error message for invalid login
+            Toast.makeText(getApplicationContext(),"Invalid login",Toast.LENGTH_SHORT).show();
+        }
     }
 }
