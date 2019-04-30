6283f7473a8c81c64b08ee310c5e16bcb0316419
bournafide <bournejocelynn@gmail.com>
2019-04-30 20:37:04 +1200

    login activity changes for code efficiency

--- app/src/main/java/com/example/alison/autprime/MainActivity.java
+++ app/src/main/java/com/example/alison/autprime/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.alison.autprime;
 //main activity contains login activity for the app
 //so when app launches, a login screen is the first thing the user will see
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,33 +15,22 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        username = findViewById(R.id.placeUsername);
-        password = findViewById(R.id.placePassword);
+        username = findViewById(R.id.placeHolderUsername);
+        password = findViewById(R.id.placeHolderPassword);
         login_button = findViewById(R.id.loginButton);
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //need to add code to verify user login details
-                //is correct with microsoft outlook before homepage is opened
                 validate(username.getText().toString(),password.getText().toString());
             }
         });
     }
-    //simple validating test for users network login and password
-    //it does not however, validate actual users network login and password stored in Microsoft Outlook
     public void validate(String username, String password){
         if(!(username.isEmpty()) && !(password.isEmpty())){
-            //print out non-error message and startup the next activity after login successful
             Toast.makeText(getApplicationContext(),"Login successful",Toast.LENGTH_SHORT).show();
-            EditText nameText = findViewById(R.id.placeUsername);
-            String text = nameText.getText().toString();
-            Intent intent = new Intent(this, Activity2.class);
-            intent.putExtra("Username", text);
-            startActivity(intent);
         }
         else{
-            //print out error message for invalid login
-            Toast.makeText(getApplicationContext(),"Invalid login",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(),"Invalid Login",Toast.LENGTH_SHORT).show();
         }
     }
 }
