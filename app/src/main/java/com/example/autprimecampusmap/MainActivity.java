a21945715da89b5350b7a45db16a3b6766262dad
HOME <johnldh1997@gmail.com>
2019-04-30 22:16:18 +1200

    Initial commit

---
+++ app/src/main/java/com/example/autprimecampusmap/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.autprimecampusmap;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Camera;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback
+{
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    private static final int REQUEST_CODE = 101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        fetchLastLocation();
+    }
+
+    private void fetchLastLocation()
+    {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                    {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>()
+        {
+            @Override
+            public void onSuccess(Location location)
+            {
+                if(location != null)
+                {
+                    currentLocation = location;
+                    Toast.makeText(getApplicationContext(), currentLocation.getLatitude() + "" + currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+                    SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
+                    supportMapFragment.getMapAsync(MainActivity.this);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap)
+    {
+        // Adding a marker on current location and camedra zooms in for current location
+        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("My current location");
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5));
+        googleMap.addMarker(markerOptions);
+
+        // Add a marker in 3 different AUT campus and move the camera
+        LatLng AUTSouthCampus = new LatLng(-36.984360, 174.879210);
+        googleMap.addMarker(new MarkerOptions().position(AUTSouthCampus).title("Marker in AUT south campus"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(AUTSouthCampus));
+
+        LatLng AUTCityCampus = new LatLng(-36.852631, 174.766785);
+        googleMap.addMarker(new MarkerOptions().position(AUTCityCampus).title("Marker in AUT city campus"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(AUTCityCampus));
+
+        LatLng AUTNorthCampus = new LatLng(-36.792930, 174.747960);
+        googleMap.addMarker(new MarkerOptions().position(AUTNorthCampus).title("Marker in AUT north campus"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(AUTNorthCampus));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        switch (requestCode)
+        {
+            case REQUEST_CODE:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    fetchLastLocation();
+                }
+                break;
+        }
+    }
+}
