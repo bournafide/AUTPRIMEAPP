655f0422f4d50910a7ede67ac324c6b75476aa2e
bournafide <bournejocelynn@gmail.com>
2019-05-06 17:26:59 +1200

    login activity commit changes with webview

--- app/src/main/java/com/example/alison/autprime/MainActivity.java
+++ app/src/main/java/com/example/alison/autprime/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -16,7 +18,7 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        username = findViewById(R.id.placeHolderUsername);
+        /*username = findViewById(R.id.placeHolderUsername);
         password = findViewById(R.id.placeHolderPassword);
         login_button = findViewById(R.id.loginButton);
         login_button.setOnClickListener(new View.OnClickListener() {
@@ -24,14 +26,31 @@ public class MainActivity extends AppCompatActivity{
             public void onClick(View view) {
                 validate(username.getText().toString(),password.getText().toString());
             }
-        });
+        });*/
+        WebView simpleWebView = (WebView) findViewById(R.id.simpleWebView);
+
+        simpleWebView.setWebViewClient(new MyWebViewClient());
+
+        String url = "https://blackboard.aut.ac.nz/webapps/login/?action=relogin";
+        simpleWebView.getSettings().setJavaScriptEnabled(true);
+        simpleWebView.loadUrl(url); // load the url on the web view
+    }
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            Toast.makeText(MainActivity.this, url, Toast.LENGTH_LONG).show();
+            // load the url
+            view.setVisibility(View.INVISIBLE);
+            return true;
+        }
     }
     //Unit testing section: Remember to change the validate method from public void to public String to start unit testing
     //Also remember to remove the (comments) double slash from the context of MainActivity below as well as the return strings in the validate method
 
     //public MainActivity(Context context){}
 
-    public void validate(String username, String password){
+    /*public void validate(String username, String password){
         if(!(username.isEmpty()) && !(password.isEmpty())){
             Toast.makeText(getApplicationContext(),"Login successful",Toast.LENGTH_SHORT).show();
             //return "Login was successful";
@@ -40,5 +59,5 @@ public class MainActivity extends AppCompatActivity{
             Toast.makeText(getApplicationContext(),"Invalid Login",Toast.LENGTH_SHORT).show();
             //return "Login was unsuccessful";
         }
-    }
+    }*/
 }
